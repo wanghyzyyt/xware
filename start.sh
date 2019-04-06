165b92d73c52f11fca6705bb1833f7d267e865b0
root <root@DESKTOP-FSLGQS3.localdomain>
2019-04-06 22:21:16 +0800

    fix portal path

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 while true; do
     pid=`ps -ef|grep vod_httpserver|grep -v grep|awk '{print $2}'`
     if [ -z "$pid" ]; then
-        ./bin/portal
+        ./portal
     else
         sleep 60
     fi
